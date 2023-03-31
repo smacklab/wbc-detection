776338103b5a8482eb729e03f39458a8e51281ec
JiraphatJack <jrpjmt@gmail.com>
2023-03-30 19:27:50 -0700

    add confidence checking

--- extract.py
+++ extract.py
@@ -14,20 +14,24 @@ for filename in os.listdir("data"):
     if not os.path.exists("wbc"):
         os.makedirs("wbc")
     
-    results = model(f)
+    results = model(f, save = True)
     for i, result in enumerate(results):
+        # conf = result.confidence.cpu().numpy()[0]
+        # print("Confidence: {}".format(conf))
         boxes = result.boxes
         xyxy = boxes.xyxy.numpy()
         for find in xyxy:
+            if (xyxy[4] < 0.25):
+                continue
             left, top, right, bottom = int(find[0]), int(find[1]), int(find[2]), int(find[3])
             width = abs(left - right)
             height = abs(top - bottom)
             if (width/height < 0.7) or (height/width < 0.7) :
                 continue
-            left -= width/2
-            top -= height/2
-            right += width/2
-            bottom += height/2
+            left -= 20
+            top -= 20
+            right += 20
+            bottom += 20
             if left < 0:
                 left = 0
             if top < 0:

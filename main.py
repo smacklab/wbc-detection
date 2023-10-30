d6b62600dd3c05db27204f0f9b0f6c6d14c266f0
yunxxxx <81000984+yunxxxx@users.noreply.github.com>
2023-10-29 23:46:08 -0700

    smear_model update and bug fixed
    fixed the bug that the code won't be able to run after 8.0.20

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from ultralytics import YOLO
 import os
+from ultralytics import YOLO
 import numpy as np
 import tifffile
 from PIL import Image
 from tqdm import tqdm
 
-
-wbc_model = YOLO("wbc-model-Feb24.pt")
 model = YOLO("blood_smear_model_Oct20.pt")
 if not os.path.exists("output"):
     os.makedirs("output")
+
 
 def create_output_folders(image_name):
     if not os.path.exists("output/" + image_name):
@@ -19,6 +18,17 @@ def create_output_folders(image_name):
     for class_name in model.names.values():
         if not os.path.exists("output/" + image_name + "/" + class_name):
             os.makedirs("output/" + image_name + "/" + class_name)
+
+
+def save_to_folder(results, cropped_image, cropped_image_name):
+    max_index = results[0].probs.top1
+    class_name = model.names[max_index]
+    if max_index == 1:
+        save_path = os.path.join("output", image_name,
+                                 class_name, cropped_image_name)
+        cropped_image.save(save_path, "JPEG", quality=100)
+    # save image to folder
+
 
 
 if __name__ == "__main__":
@@ -40,41 +50,8 @@ if __name__ == "__main__":
                 for r in tqdm(range(0, h, 512), leave=False):
                     cropped_image = image.crop((c, r, c + 512, r + 512))
                     cropped_image_name = image_name + \
-                        "_cropped_" + str(c) + "_" + str(r)
+                        "_cropped_" + str(c) + "_" + str(r) + ".jpg"
 
                     # run prediction on image
                     results = model(cropped_image)
-                    max_index = results[0].probs.top1
-                    class_name = model.names[max_index]
-                    if class_name == 'Dense':
-                        continue
-                    if max_index == 1:
-                        results = wbc_model(cropped_image)
-                        for result in results:
-                            boxes = result.boxes
-                            for i, box in enumerate(boxes):
-                                if box.conf < 0.25:
-                                    continue
-                                xyxy = box.xyxy.numpy()
-                                for find in xyxy:
-                                    left, top, right, bottom = int(find[0]), int(find[1]), int(find[2]), int(find[3])
-                                    width = abs(left - right)
-                                    height = abs(top - bottom)
-                                    if (width/height < 0.7) or (height/width < 0.7) :
-                                        continue
-                                    left -= 50
-                                    top -= 50
-                                    right += 50
-                                    bottom += 50
-                                    if left < 0:
-                                        left = 0
-                                    if top < 0:
-                                        top = 0
-                                    if right > cropped_image.size[0]:
-                                        right = cropped_image.size[0]
-                                    if bottom > cropped_image.size[1]:
-                                        bottom = cropped_image.size[1]
-                                    image1 = cropped_image.crop((left, top, right, bottom))
-                                    save_path = os.path.join("output", image_name,
-                                                            class_name,  cropped_image_name + str(i) + ".jpg")
-                                    image1.save(save_path, "JPEG", quality=100)
+                    save_to_folder(results, cropped_image, cropped_image_name)
